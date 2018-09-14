b06ad8330a2efccff44b7939798c852853d4724d
Korenev Konstatyn <korenev@iled.com.ua>
2018-09-14 08:34:37 +0300

    first commit

---
+++ sql/znaniesila_db.sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Сен 14 2018 г., 08:25
+-- Версия сервера: 5.6.38
+-- Версия PHP: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `znaniesila_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` smallint(5) UNSIGNED NOT NULL,
+  `category` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `category`) VALUES
+(1, 'article');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `users_id` int(10) UNSIGNED NOT NULL,
+  `transaction_id` varchar(45) NOT NULL,
+  `payment_status` varchar(45) NOT NULL,
+  `payment_amount` int(10) UNSIGNED NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `categories_id` smallint(5) UNSIGNED NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` tinytext NOT NULL,
+  `content` longtext,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `pages`
+--
+
+INSERT INTO `pages` (`id`, `categories_id`, `title`, `description`, `content`, `date_created`) VALUES
+(1, 1, 'Хлоргекседину біглюконат', 'Хлоргекседину біглюконат', '<p>Хлоргекседину біглюконат</p>', '2018-08-14 13:55:50');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `pdfs`
+--
+
+CREATE TABLE `pdfs` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` tinytext NOT NULL,
+  `tmp_name` char(63) NOT NULL,
+  `file_name` varchar(100) NOT NULL,
+  `size` mediumint(8) UNSIGNED NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `pdfs`
+--
+
+INSERT INTO `pdfs` (`id`, `title`, `description`, `tmp_name`, `file_name`, `size`, `date_created`) VALUES
+(3, 'Протокол №205', 'Протокол к годовому плану закупок', '257d38efa9561b3fc832edadbe96e0c798a045355b7ac6bd79fe12.26784371', 'Протокол №205 Бензин А-92 1960 грн..pdf', 1057, '2018-08-20 13:48:45');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `type` enum('member','admin') NOT NULL DEFAULT 'member',
+  `username` varchar(45) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_expires` date NOT NULL,
+  `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `type`, `username`, `email`, `pass`, `first_name`, `last_name`, `date_created`, `date_expires`, `date_modified`) VALUES
+(1, 'admin', 'konkore', 'konkore@meta.ua', '$2y$10$3OJqs/hMGibCRIvnu03WluwcE4JHOpQkv/NRvmGQyaH8Uo2u4Jaiu', 'Константин', 'Коренев', '2018-08-07 12:04:10', '2020-12-31', '2018-08-17 06:08:50');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `categoryJJNIQUE` (`category`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `date_created` (`date_created`),
+  ADD KEY `transaction_id` (`transaction_id`);
+
+--
+-- Индексы таблицы `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `date_created` (`date_created`),
+  ADD KEY `fk_pages_categories_idx` (`categories_id`);
+
+--
+-- Индексы таблицы `pdfs`
+--
+ALTER TABLE `pdfs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tmp_name_UNIQUE` (`tmp_name`),
+  ADD KEY `date_created` (`date_created`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username_UNIQUE` (`username`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD KEY `login` (`email`,`pass`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `pdfs`
+--
+ALTER TABLE `pdfs`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_orders_usersl` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `pages`
+--
+ALTER TABLE `pages`
+  ADD CONSTRAINT `fk_pages_categories` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
